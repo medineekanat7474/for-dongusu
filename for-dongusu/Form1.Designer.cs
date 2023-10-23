7258c349c768e4d94d4148335f61f5f3e5cee424
medineekanat7474 <145452232+medineekanat7474@users.noreply.github.com>
2023-10-23 13:17:26 +0300

    Add files via upload

---
+++ for-dongusu/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace for_dongusu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBaslangic = new System.Windows.Forms.TextBox();
+            this.txtBitis = new System.Windows.Forms.TextBox();
+            this.lstSayi = new System.Windows.Forms.ListBox();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(43, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Başlangıç";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(78, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bitiş";
+            // 
+            // txtBaslangic
+            // 
+            this.txtBaslangic.Location = new System.Drawing.Point(119, 32);
+            this.txtBaslangic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBaslangic.Name = "txtBaslangic";
+            this.txtBaslangic.Size = new System.Drawing.Size(132, 22);
+            this.txtBaslangic.TabIndex = 2;
+            // 
+            // txtBitis
+            // 
+            this.txtBitis.Location = new System.Drawing.Point(119, 90);
+            this.txtBitis.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBitis.Name = "txtBitis";
+            this.txtBitis.Size = new System.Drawing.Size(132, 22);
+            this.txtBitis.TabIndex = 3;
+            // 
+            // lstSayi
+            // 
+            this.lstSayi.FormattingEnabled = true;
+            this.lstSayi.ItemHeight = 16;
+            this.lstSayi.Location = new System.Drawing.Point(328, 17);
+            this.lstSayi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstSayi.Name = "lstSayi";
+            this.lstSayi.Size = new System.Drawing.Size(159, 196);
+            this.lstSayi.TabIndex = 4;
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGoster.Location = new System.Drawing.Point(119, 155);
+            this.btnGoster.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(133, 58);
+            this.btnGoster.TabIndex = 5;
+            this.btnGoster.Text = "GÖSTER";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 247);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.lstSayi);
+            this.Controls.Add(this.txtBitis);
+            this.Controls.Add(this.txtBaslangic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBaslangic;
+        private System.Windows.Forms.TextBox txtBitis;
+        private System.Windows.Forms.ListBox lstSayi;
+        private System.Windows.Forms.Button btnGoster;
+    }
+}
+
---
+++ for-dongusu/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_dongusu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            int baslangic=Convert.ToInt32(txtBaslangic.Text);
+            int bitis=Convert.ToInt32(txtBitis.Text);
+            for (int i = baslangic; i <=bitis; i++)
+            {
+                lstSayi.Items.Add(i);
+            }
+        }
+    }
+}
